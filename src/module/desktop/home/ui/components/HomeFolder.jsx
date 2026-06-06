db7476e69bfec1759434f8078e44e162b8af5cc1
Kuldeep <kunalrajput67718@gmail.com>
2026-06-06 22:46:49 +0530

    feat: truncate folder display name and remove hover styling in desktop home component

--- src/module/desktop/home/ui/components/HomeFolder.jsx
+++ src/module/desktop/home/ui/components/HomeFolder.jsx
@@ -1,13 +1,13 @@
 import clsx from "clsx";
 
-const HomeFolder = ({ project, onClick }) => (
-  <li
-    className={clsx("group folder", project.windowPosition)}
-    onClick={onClick}
-  >
-    <img src="/images/folder.png" alt={project.name} />
-    <p>{project.name}</p>
-  </li>
-);
+const HomeFolder = ({ project, onClick }) => {
+  const displayName = project.name === "Resume Ats Scanner" ? "Resume ATS" : project.name;
+  return (
+    <li className={clsx("group folder", project.windowPosition)} onClick={onClick}>
+      <img src="/images/folder.png" alt={displayName} />
+      <p>{displayName}</p>
+    </li>
+  );
+};
 
 export default HomeFolder;
