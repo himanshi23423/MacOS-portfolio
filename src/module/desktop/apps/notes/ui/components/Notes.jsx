3cb672982a98949c199dffc8191eec388f62ba5a
Kuldeep <kunalrajput67718@gmail.com>
2026-06-16 22:59:23 +0530

    refactor: update import path for useNotes hook in Notes component

--- src/module/desktop/apps/notes/ui/components/Notes.jsx
+++ src/module/desktop/apps/notes/ui/components/Notes.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import windowWrapper from "@hoc/windowWrapper";
 import useWindowsStore from "@store/window";
-import useNotes from "./useNotes";
+import useNotes from "../../hooks/useNotes";
 import NotesSection from "../section/NotesSection";
 import NotesAboutModal from "./NotesAboutModal";
 
